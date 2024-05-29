8e2f4558533d6d7ef96ddea7857c2520b9dff6e8
Nacho Menéndez <nchmnndzlpz@gmail.com>
2024-05-29 11:13:40 +0200

    fix alt imagen logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo/logo.webp";
 const Header = () => {
   return (
     <header>
-      <img src={logo} alt="Logo de El Mundo de Jonathan, representado por una j mayúscula sobre una ilustración de la bola del mundo" />
+      <img src={logo} alt="Logo de El Mundo de Jonathan, representado por la letra j mayúscula sobre una ilustración de la bola del mundo" />
       <h1>El mundo de Jonathan</h1>
     </header>
   )
